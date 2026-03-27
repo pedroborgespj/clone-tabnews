cd1efbb33a7dde83d6252da44c4aa2a8bd3f3b4d
Pedro Borges <53882509+pedroborgespj@users.noreply.github.com>
2026-03-27 13:48:39 +0000

    finalizando a página "Em Construção"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Pedro ❤️ Laura ❤️ Beth 🐕</h1>;
+  return (
+    <>
+      <h1>Pedro ❤️ Laura ❤️ Beth 🐕</h1>
+      <strong>Em construção: estamos trabalhando em novas ideias 🚧👷</strong>
+    </>
+  );
 }
 
 export default Home;
